fb2484fd9ba056701e767e1f87596fc7abe41bbe
robiul0023 <asiq007@gmail.com>
2024-09-04 00:21:40 +0600

    Rinterio News section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,13 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script>
-    tailwind.config = {
-      theme: {
-        extend: {
-          colors: {
-            clifford: '#da373d',
-          }
-        }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
       }
-    }
-  </script>
-</head>
-<body>
-  <h1 class="text-3xl font-bold underline **text-clifford**">
-    Hello world!
-  </h1>
-</body>
-</html>
+    },
+  },
+  plugins: [],
+}
